24931596e45b2adfc11daa4307ed294d86e09fd7
Ozanne Alexander <alexanoz@outlook.com>
2023-01-09 23:30:03 -0500

    added function for the license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none"){
+    return `*[License](license)`
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
